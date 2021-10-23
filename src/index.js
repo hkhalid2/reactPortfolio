7eb2f5cca752310424b6c668c4d63124dfadc025
Hamza Khalid <hamzer95@gmail.com>
2021-10-23 15:10:08 -0400

    deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => <PortfolioContainer />;
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
