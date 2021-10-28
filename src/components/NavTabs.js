273d6e69698c9168186390731525a818a4367b9e
Hamza Khalid <hamzer95@gmail.com>
2021-10-27 22:09:35 -0400

    added to portfolio

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Nav, Container } from 'react-bootstrap';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
+    <Nav className="navbar navbar-dark bg-dar">
       <li className="nav-item">
         <a
           href="#about"
@@ -45,7 +46,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           Contact
         </a>
       </li>
-    </ul>
+    </Nav>
   );
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Screenshot1 from '../../images/screenshot1.png';
 import Screenshot2 from '../../images/screenshot2.png';
 import DemoGif from '../../images/demo2.gif';
+import Demo2Gif from '../../images/demo.gif';
 import ProjectShot from '../../images/project.png';
+import BudgetShot from '../../images/screenshot.png';
+import WeatherGif from '../../images/weather.gif';
 import '../../styles/style.css';
 
 
@@ -43,6 +46,33 @@ export default function Blog() {
                     <a href="https://github.com/endlessashley/Shield-ID">Link to Github Repository</a>
                     <br />
                     <img class="container-sm" src={ProjectShot} width="300" height="300"/>
+                </div>
+
+                <div class="container-md">
+                    <h5>- Budget Tracker</h5>
+                    <a href="https://obscure-reef-64359.herokuapp.com">Link to Live Deployment</a>
+                    <br />
+                    <a href="https://github.com/hkhalid2/budgetTracker">Link to Github Repository</a>
+                    <br />
+                    <img class="container-sm" src={BudgetShot} width="300" height="300"/>
+                </div>
+
+                <div class="container-md">
+                    <h5>- Work Day Scheduler</h5>
+                    <a href="https://hkhalid2.github.io/Homework5WorkDaySched/ ">Link to Live Deployment</a>
+                    <br />
+                    <a href="https://github.com/hkhalid2/Homework5WorkDaySched">Link to Github Repository</a>
+                    <br />
+                    <img class="container-sm" src={Demo2Gif} width="300" height="300"/>
+                </div>
+
+                <div class="container-md">
+                    <h5>- Weather Dashboard</h5>
+                    <a href="https://hkhalid2.github.io/weatherDashboard/">Link to Live Deployment</a>
+                    <br />
+                    <a href="https://github.com/hkhalid2/weatherDashboard ">Link to Github Repository</a>
+                    <br />
+                    <img class="container-sm" src={WeatherGif} width="300" height="300"/>
                 </div>
 
 
