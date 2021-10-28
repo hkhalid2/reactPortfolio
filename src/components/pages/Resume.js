424d826488e562b9a7e8065a0505a024005a4458
Hamza Khalid <hamzer95@gmail.com>
2021-10-27 22:19:47 -0400

    added proficiencies

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,26 @@ export default function Resume() {
         <><h1>Resume </h1><section id="Resume" class="container">
 
             <div class="text-block">
+                <p>Developer's Proficiencies: </p>
+                <ul>
 
+                    <li> CSS3</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Express.js</li>
+                    <li>React.js</li>
+                    <li>Node.js</li>
+                    <li>Progressive Web Apps</li>
+                    <li>Agile Methodology</li>
+                    <li>Computer Science</li>
+                    <li>Database Theory</li>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                    <li>Git</li>
+
+
+                </ul>
+                <p>For full resume click below. </p>
                 <p class="container">
                     <a href="https://drive.google.com/file/d/1zmLnOcQnnsNMQjj0pie1JgstY399hn63/view?usp=sharing">Resume</a>
                 </p>
